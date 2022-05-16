a73d9dd1c5c2866c70cc712d48da5637c2ed0b1e
Joevenable <binechorighteous@gmail.com>
2022-05-16 12:38:52 -0500

    integrated material UI

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,3 +1,4 @@
+import { Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import Catalog from "../../features/catalog/Catalog";
 import { Product } from "./models/product";
@@ -23,11 +24,11 @@ function App() {
       }])
   }
   return (
-    <div>
-      <h1>Store</h1>
+    <>
+      <Typography variant='h1'>Store</Typography>
       <Catalog products={products} addProduct={addProduct} />
 
-    </div>
+    </>
   );
 }
 
